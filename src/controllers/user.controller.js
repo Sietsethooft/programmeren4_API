79f1d56d3eccb47ad35013bc9862bd6e07f0d359
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-05-02 00:24:32 +0200

    registerUser Controller
    Made the first version of the UC-201 controller code:
    
    Logs the received user data and then uses the userService.
    When there is a error, it will send it to the error handler in the map util.
    
    TO DO: Add validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,20 @@ const userService = require('../services/user.services');
 const logger = require('../util/Logger');
 
 const userController = {
+    registerUser: (req, res, next) => {
+        const userData = req.body;
+        logger.info('Received user data:', userData); // Logs the received user data
     
+        userService.registerUser(userData, (err, result) => {
+            if (err) return next(err); // This sends the error to the error handler in util.
+    
+            res.status(201).json({
+                status: 201,
+                message: 'User registered successfully',
+                data: result
+            });
+        });
+    }   
 };
 
 module.exports = userController;
