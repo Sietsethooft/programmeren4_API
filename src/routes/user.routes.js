891fbd95deaa045126e0b82c6021fe15667a3e89
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-05-01 23:38:59 +0200

    Creation UC-201 route
    Made the route to the controller for UC-201
    
    Only /user, because the index.js has the /api part.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,4 +3,6 @@ const userController = require('../controllers/user.controller');
 
 const userRoutes = express.Router();
 
+userRoutes.post('/user', userController.registerUser);
+
 module.exports = userRoutes;
