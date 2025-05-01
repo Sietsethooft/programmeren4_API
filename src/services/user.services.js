4a1a421d1526e8839f198ae543dceeef2c868b30
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-05-02 00:33:12 +0200

    UC-201 service
    Made the service for UC-201. First it splits the userdata, so the information will be correct in the query.
    
    Then it activates the query.
    
    When user is succesfully registerd, it will send all the information back with the new userId.
    
    TO DO:
    Make connection with errorhandler

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -2,6 +2,33 @@ const logger = require('../util/Logger');
 const db = require('../database/DBconfig');
 
 const userServices = {
+    registerUser: (userData, callback) => {
+        const { firstName, lastName, street, city, emailAdress, password, phonenumber } = userData; // Destructure userData
+    
+        const query = `
+            INSERT INTO user (firstName, lastName, street, city, emailAdress, password, phonenumber)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+        `;
+    
+        db.query(query, [firstName, lastName, street, city, emailAdress, password, phonenumber], (error, results) => {
+            if (error) {
+                logger.error('Error inserting user into database:', error.message);
+                return callback(error, null);
+            }
+    
+            logger.info('User registered successfully:', results.insertId);
+            return callback(null, {
+                userId: results.insertId,
+                firstName,
+                lastName,
+                street,
+                city,
+                emailAdress,
+                password,
+                phonenumber
+              });
+        });
+    }    
 };
 
 module.exports = userServices;
