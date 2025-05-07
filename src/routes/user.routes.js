eb088056cc378b4ddb3356d401a7c5bd0fb702da
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-05-07 18:11:13 +0200

    Creation of UC-203 route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,6 +6,7 @@ const userRoutes = express.Router();
 userRoutes.post('/user', userController.registerUser);
 
 userRoutes.get('/user', authMiddleware.authenticateToken, userController.getAllUsers);
+userRoutes.get('/user/profile', authMiddleware.authenticateToken, userController.getUserProfile);
 userRoutes.get('/user/:userId', authMiddleware.authenticateToken, userController.getUserById);
 
 userRoutes.put('/user/:userId', authMiddleware.authenticateToken, userController.updateUser);
