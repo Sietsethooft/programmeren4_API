5ed5c830b0c4cccc38b34055b2cb60ea173b9e83
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-05-07 16:38:23 +0200

    Auth middleware creation
    Made the auth middleware that verifys the given token.

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+const logger = require('../util/Logger');
+
+const authMiddleware = {
+    authenticateToken: (req, res, next) => {
+        const authHeader = req.headers['authorization']; // Get the authorization header from the request
+        logger.trace('Authorization header:', authHeader);
+
+        const token = authHeader && authHeader.split(' ')[1]; // Extract the token from the header
+        logger.trace('Extracted token:', token);
+
+        if (!token) {
+            return res.status(401).json({
+                status: 401,
+                message: 'Access token is missing or invalid.',
+                data: {}
+            });
+        }
+
+        jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+            if (error) {
+                logger.error('Invalid token:', err.message);
+                return res.status(403).json({
+                    status: 403,
+                    message: 'Invalid or expired token.',
+                    data: {}
+                });
+            }
+
+            req.user = user; // Attach the user information to the request object
+            next();
+        });
+    }
+};
+
+module.exports = authMiddleware;
