7358eb41901c79a6bec4b6ae5dafe2010ac124e2
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-05-08 23:17:09 +0200

    createMeal service
    Made the service for the UC301. Now it imports the meal in the database

--- src/services/meal.services.js
+++ src/services/meal.services.js
@@ -2,6 +2,24 @@ const logger = require('../util/Logger');
 const db = require('../database/DBconfig');
 
 const mealService = {
+    createMeal: (mealData, loggedInUserId, callback) => { // UC-301
+        const { name, description, price, dateTime, maxAmountOfParticipants, imageUrl } = mealData;
+
+        const query = `INSERT INTO meal (name, description, price, dateTime, maxAmountOfParticipants, imageUrl, cookId) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+        const params = [name, description, price, dateTime, maxAmountOfParticipants, imageUrl, loggedInUserId];
+
+        db.query(query, params, (error, result) => {
+            if (error) return callback(error);
+
+            const createdMeal = {
+                id: result.insertId,
+                ...mealData,
+                cookId: loggedInUserId,
+            };
+
+            callback(null, createdMeal);
+        });
+    }
 }
 
 module.exports = mealService;
