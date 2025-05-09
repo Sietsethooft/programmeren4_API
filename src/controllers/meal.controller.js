5d33193e239651d83173712c5fc63abc993fedc8
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-05-09 19:05:31 +0200

    UC302 controller creation
    Same as the UC305, but also with a mealvalidation, so the systems checks if you enterd all required information

--- src/controllers/meal.controller.js
+++ src/controllers/meal.controller.js
@@ -27,6 +27,52 @@ const mealController = {
         });
     },
 
+    updateMeal: (req, res, next) => { // UC-302
+        const mealId = parseInt(req.params.mealId, 10);
+        const loggedInUserId = req.user.userId; // Get the userId from the token
+        const mealData = req.body;   
+
+        const { error } = validation.updateMealValidation(mealData); // Validate the meal data
+            if (error) {
+                return res.status(400).json({
+                    status: 400,
+                    message: error.message,
+                    data: {}
+                });
+            }
+
+        mealService.getCookId(mealId, (error, cookId) => {
+            if (error) return next(error); // This sends the error to the error handler in util.
+
+            if (!cookId) {
+                return res.status(404).json({
+                    status: 404,
+                    message: 'Meal not found',
+                    data: {}
+                });
+            }
+
+            if (loggedInUserId !== cookId) {
+                return next({
+                    status: 403,
+                    message: "User is not the owner of this meal",
+                    data: {}
+                });
+            }
+
+            mealService.updateMeal(mealId, mealData, (error, result) => {
+                if (error) return next(error); // This sends the error to the error handler in util.
+
+                res.status(200).json({
+                    status: 200,
+                    message: 'Meal updated successfully',
+                    data: result
+                });
+            });
+
+        });
+    },
+
     getAllMeals: (req, res, next) => { // UC-303
         mealService.getAllMeals( (error, meals) => {
             if (error) return next(error); // This sends the error to the error handler in util.
@@ -39,7 +85,7 @@ const mealController = {
         });
     },
 
-    getMealById: (req, res, next) => { // UC-302
+    getMealById: (req, res, next) => { // UC-304
         const mealId = parseInt(req.params.mealId, 10);
 
         mealService.getMealById(mealId, (error, result) => {
