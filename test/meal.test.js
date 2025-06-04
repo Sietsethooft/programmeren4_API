378ed2b970f9aebb4360528a002ab7ce8b5d2545
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-06-04 17:12:23 +0200

    Basic test requirements

---
+++ test/meal.test.js
@@ -0,0 +1,12 @@
+process.env.DB_DATABASE = 'share-a-meal-testdb';
+process.env.JWT_SECRET = process.env.JWT_SECRET || 'secret';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../index');
+const e = require('express');
+logger = require('../src/util/Logger');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
