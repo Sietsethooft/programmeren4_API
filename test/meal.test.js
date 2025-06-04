ac0acbdae488ec17a4f269f15e2abc05356cfd4e
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-06-04 17:36:26 +0200

    Token update
    Made a before and after, so you get a token a userId of a new user for the meal tests

--- test/meal.test.js
+++ test/meal.test.js
@@ -9,4 +9,50 @@ logger = require('../src/util/Logger');
 
 const expect = chai.expect;
 
+const userEmail = 'j.klaasen@test.com';
+const userPassword = 'ValidPassword1234';
+let userId;
+let token;
+
 chai.use(chaiHttp);
+
+before((done) => {
+  chai.request(app)
+    .post('/api/user')
+    .send({ 
+        firstName: 'Jan', 
+        lastName: 'Klaasen', 
+        street: '123 Main St', 
+        city: 'Anytown', 
+        emailAdress: userEmail,
+        password: userPassword,
+        phonenumber: '06-12345678'})
+    .end(() => {
+      chai.request(app)
+        .post('/api/login')
+        .send({
+            emailAdress: userEmail,
+            password: userPassword
+        })
+        .end((err, res) => {
+          token = res.body.data.token;
+          userId = res.body.data.user.id;
+          logger.info(`Token: ${token}`);
+          logger.info(`User ID: ${userId}`);
+          done();
+        });
+    });
+});
+
+after((done) => {
+    if (userId && token) {
+        chai.request(app)
+            .delete(`/api/user/${userId}`)
+            .set('Authorization', `Bearer ${token}`)
+            .end(() => {
+                done();
+            });
+    } else {
+        done();
+    }
+});
