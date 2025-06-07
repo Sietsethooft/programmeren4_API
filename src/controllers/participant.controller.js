386e371b84d1b1bb39f1bb623b44266c9b7b4680
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-06-07 13:20:36 +0200

    Creating basic controller

--- src/controllers/participant.controller.js
+++ src/controllers/participant.controller.js
@@ -2,6 +2,8 @@ const participantService = require('../services/participant.services');
 const logger = require('../util/Logger');
 
 const participantController = {
+    createParticipant: (req, res, next) => { // UC-401
+    }
 }
 
 module.exports = participantController;
