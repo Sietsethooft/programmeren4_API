e6078980cb3f48c9b0c2e06369eca28725944806
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-06-07 13:20:21 +0200

    Creation basic service

--- src/services/participant.services.js
+++ src/services/participant.services.js
@@ -2,6 +2,8 @@ const logger = require('../util/Logger');
 const db = require('../database/DBconfig');
 
 const participantService = {
+    createParticipant: (mealId, loggedInUserId, callback) => {
+    }
 }
 
 module.exports = participantService;
