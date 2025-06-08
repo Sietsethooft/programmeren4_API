88a1304a14550fec9c7aeecab3f5887de27f7a3c
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-06-08 15:27:36 +0200

    Creation createParticipant service code (UC-401)

--- src/services/participant.services.js
+++ src/services/participant.services.js
@@ -3,6 +3,14 @@ const db = require('../database/DBconfig');
 
 const participantService = {
     createParticipant: (mealId, loggedInUserId, callback) => {
+        const query = 'INSERT INTO meal_participants_user (mealId, userId) VALUES (?, ?)';
+        db.query(query, [mealId, loggedInUserId], (error, results) => {
+            if (error) {
+                logger.error(`Error creating participant: ${error.message}`);
+                return callback(error);
+            }
+            callback(null, results.mealId, results.userId);
+        });
     }
 }
 
