e49ffb36738756b06385f67e1f5a7ebcbe7ba9b1
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-06-08 15:51:13 +0200

    Creation UC402 controller

--- src/controllers/participant.controller.js
+++ src/controllers/participant.controller.js
@@ -42,6 +42,47 @@ const participantController = {
                 });
             });
         });
+    },
+
+    deleteParticipant: (req, res, next) => { // UC-402
+        const mealId = parseInt(req.params.mealId, 10);
+        const loggedInUserId = req.user.userId; // Get the userId from the token
+
+        mealService.getMealById(mealId, (error, meals) => {
+            if (error) return next(error); // This sends the error to the error handler in util.
+
+            if (!meals || meals.length === 0) { // Check if the meal exists
+                logger.warn(`Meal with ID #${mealId} not found`);
+                return res.status(404).json({
+                    status: 404,
+                    message: `Meal not found.`,
+                    data: {}
+                });
+            }
+
+            participantService.checkParticipant(mealId, loggedInUserId, (error, participant) => {
+                if (error) return next(error);
+
+                if (!participant || participant.length === 0) { // Check if the user is a participant of the meal
+                    logger.warn(`User with ID #${loggedInUserId} is not a participant of meal with ID #${mealId}`);
+                    return res.status(404).json({
+                        status: 404,
+                        message: `User is not a participant of this meal.`,
+                        data: {}
+                    });
+                }
+
+                participantService.deleteParticipant(mealId, loggedInUserId, (error, result) => {
+                    if (error) return next(error); // This sends the error to the error handler in util.
+                    logger.info(`User with ID #${loggedInUserId} has unsubscribed from meal with ID #${mealId}`);
+                    res.status(200).json({
+                        status: 200,
+                        message: `User with ID #${loggedInUserId} has unsubscribed from meal with ID #${mealId}`,
+                        data: {participant: result}
+                    });
+                });
+            });
+        });
     }
 }
 
