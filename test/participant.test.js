d9d1df1d643fdcdc5b018ef8cd6437e20e92befb
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-06-09 15:46:05 +0200

    Test formats update
    Added all the tests for participants in format

--- test/participant.test.js
+++ test/participant.test.js
@@ -11,3 +11,22 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
+describe('UC-401: Create Participant', () => {
+    // TC-401-1: Not authenticated
+
+    // TC-401-2: Meal not found
+
+    // TC-401-3: Successfully signed up for meal
+
+    // TC-401-4: Meal has reached maximum number of participants
+});
+
+describe('UC-402: Delete Participant', () => {
+    // TC-402-1: Not authenticated
+
+    // TC-402-2: Meal not found
+
+    // TC-402-3: User is not a participant of this meal
+
+    // TC-402-4: Successfully deleted participant
+});
