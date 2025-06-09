8ce4499b9130f6029457a617584c9a0a08eb2c99
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-06-09 16:13:11 +0200

    Before and after tests update
    Made some tests, so a user will be created with a meal.

--- test/participant.test.js
+++ test/participant.test.js
@@ -9,16 +9,62 @@ logger = require('../src/util/Logger');
 
 const expect = chai.expect;
 
-chai.use(chaiHttp);
+let token; // Variable to store the token for authenticated requests
+let mealId; // Variable to store the meal ID for participant tests
+let userId; // Variable to store the user ID for cleanup after tests
 
 describe('UC-401: Create Participant', () => {
     // TC-401-1: Not authenticated
+const userEmail = 'p.test@testcase.nl';
+const userPassword = 'Testpassword1234';
 
     // TC-401-2: Meal not found
+chai.use(chaiHttp);
 
     // TC-401-3: Successfully signed up for meal
+before((done) => { // Before all tests, create a user and log in to get a token
+  chai.request(app)
+    .post('/api/user')
+    .send({ 
+        firstName: 'Jan', 
+        lastName: 'Klaasen', 
+        street: 'Damstraat 1', 
+        city: 'Amsterdam', 
+        emailAdress: userEmail,
+        password: userPassword,
+        phonenumber: '06-12345678'})
+    .end(() => {
+      chai.request(app)
+        .post('/api/login')
+        .send({
+            emailAdress: userEmail,
+            password: userPassword
+        })
+        .end((err, res) => {
+            token = res.body.data.token;
+            userId = res.body.data.user.id;
+            done();
+        });
+    });
+});
 
-    // TC-401-4: Meal has reached maximum number of participants
+before((done) => {
+    // Create a meal to use in participant tests
+    chai.request(app)
+        .post('/api/meal')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+            name: "Spaghetti Bolognese",
+            description: "Klassieke Italiaanse pasta met rijke tomatensaus en gehakt.",
+            dateTime: "2026-05-10 18:00:00",
+            maxAmountOfParticipants: 1,
+            imageUrl: "https://spaghettifoto.com",
+            price: 10.00
+        })
+        .end((err, res) => {
+            mealId = res.body.data.meal.id; // Store the meal ID for later use
+            done();
+        });
 });
 
 describe('UC-402: Delete Participant', () => {
@@ -29,4 +75,17 @@ describe('UC-402: Delete Participant', () => {
     // TC-402-3: User is not a participant of this meal
 
     // TC-402-4: Successfully deleted participant
+after((done) => {
+    // Clean up: Delete the created meal first, then the user
+    chai.request(app)
+        .delete(`/api/meal/${mealId}`)
+        .set('Authorization', `Bearer ${token}`)
+        .end(() => {
+            chai.request(app)
+                .delete(`/api/user/${userId}`)
+                .set('Authorization', `Bearer ${token}`)
+                .end(() => {
+                    done();
+                });
+        });
 });
